bc8cbeae922f2f5e426e7616b3f8e56003d1d577
dzakki <dzakkiaz7@gmail.com>
2020-10-20 23:59:24 +0800

    add method firstEntity

--- main/services/receive.js
+++ main/services/receive.js
@@ -97,5 +97,9 @@ class Receive {
         // Send the response message
         GraphApi.callSendApi(requestBody)
     }
+
+    firstEntity(nlp, name) {
+        return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+    }
 }
 module.exports = Receive
