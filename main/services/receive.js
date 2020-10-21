39670a5aa5c93fad90e16d887e62b5c741218656
dzakki <dzakkiaz7@gmail.com>
2020-10-21 15:32:46 +0800

    fix typo

--- main/services/receive.js
+++ main/services/receive.js
@@ -55,7 +55,7 @@ class Receive {
 
         // {"intents":[],"entities":{"wit$location:location":[{"id":"624173841772436","name":"wit$location","role":"location","start":0,"end":5,"body":"hallo","confidence":0.841,"entities":[],"suggested":true,"value":"hallo","type":"value"}]},"traits":{"wit$sentiment":[{"id":"5ac2b50a-44e4-466e-9d49-bad6bd40092c","value":"positive","confidence":0.733}],"wit$greetings":[{"id":"5900cc2d-41b7-45b2-b21f-b950d3ae3c5c","value":"true","confidence":0.9915}]},"detected_locales":[{"locale":"id_ID","confidence":0.4886}]}
         // console.log(this.webhookEvent.message.nlp.traits['wit$greetings'][0].confidence, "greeting.confidence")
-        const isGetStarted = handleGetStarted(message)
+        const isGetStarted = this.handleGetStarted(message)
         if (greeting > 0.8 || isGetStarted) {
             response = isGetStarted
         }
