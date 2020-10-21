305d3f171fa1b285393dc2d057ce8f9a29ca25af
dzakki <dzakkiaz7@gmail.com>
2020-10-21 15:31:16 +0800

    handle get started in text msg

--- main/services/receive.js
+++ main/services/receive.js
@@ -49,40 +49,15 @@ class Receive {
         // console.log(this.webhookEvent.message.nlp, "this.webhookEvent.message.nlp")
         // console.log(this.webhookEvent.message, "this.webhookEvent.message")
         // check greeting is here and is confident
-        let greeting = this.firstEntity(this.webhookEvent.message.nlp, "GET STARTED");
+        let greetingConfidence = this.webhookEvent.message.nlp.traits['wit$greetings'][0].confidence;
         let message = this.webhookEvent.message.text.trim().toLowerCase();
         let response;
 
         // {"intents":[],"entities":{"wit$location:location":[{"id":"624173841772436","name":"wit$location","role":"location","start":0,"end":5,"body":"hallo","confidence":0.841,"entities":[],"suggested":true,"value":"hallo","type":"value"}]},"traits":{"wit$sentiment":[{"id":"5ac2b50a-44e4-466e-9d49-bad6bd40092c","value":"positive","confidence":0.733}],"wit$greetings":[{"id":"5900cc2d-41b7-45b2-b21f-b950d3ae3c5c","value":"true","confidence":0.9915}]},"detected_locales":[{"locale":"id_ID","confidence":0.4886}]}
-        console.log(this.webhookEvent.message.nlp.traits['wit$greetings'][0], "greeting.confidence")
-        console.log(message)
-        if (message === "GET STARTED") {
-            response = [
-                {
-                    text: `Hi ${this.user.first_name} selamat datang di Gist Bot, dimana kamu bisa menyimpan poin poin penting yang kamu punya di memori aku.`
-                },
-                {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "button",
-                            "text": "apakah kamu membutuh kan pentunjuk untuk menggunakan memori ku?",
-                            "buttons": [
-                                {
-                                    "type": "postback",
-                                    "title": "Iya!",
-                                    "payload": "petunjuk_yes",
-                                },
-                                {
-                                    "type": "postback",
-                                    "title": "Tidak!",
-                                    "payload": "petunjuk_no",
-                                }
-                            ],
-                        }
-                    }
-                }
-            ]
+        // console.log(this.webhookEvent.message.nlp.traits['wit$greetings'][0].confidence, "greeting.confidence")
+        const isGetStarted = handleGetStarted(message)
+        if (greeting > 0.8 || isGetStarted) {
+            response = isGetStarted
         }
         // if (message === "help") {
         //     response = {
