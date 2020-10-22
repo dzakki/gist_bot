0f347f42e5c8a0879ea6bb1b23c4143dd00bd3e6
dzakki <dzakkiaz7@gmail.com>
2020-10-22 19:54:35 +0800

    added webview

--- main/app.js
+++ main/app.js
@@ -8,11 +8,20 @@ const
     express = require('express'),
     app = express(),
     router = require("./routes"),
+    path = require('path'),
     PORT = process.env.PORT || 1337;
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+const publicPath = path.join(__dirname, 'view');
+
+console.log(path.join(publicPath, 'index.html'))
+app.get('/webview', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.use(router);
 
